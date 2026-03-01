546b3804cf768c4fe5135282221aa9fab75eb98b
clark raguhos <clarkkentraguhos@gmail.com>
2026-03-01 19:01:23 +0800

    fixed the cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8000', 'http://127.0.0.1:8000'],
+    'allowed_origins' => ['https://barangaywestrembo.onrender.com/'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true, 
 ];
