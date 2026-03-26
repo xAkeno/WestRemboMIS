86a9b1be5839bdfc9b6bc44f92153cba07bd0a46
clark raguhos <clarkkentraguhos@gmail.com>
2026-03-26 16:21:07 +0800

    added the logging

---
+++ config/logging.php
@@ -0,0 +1,27 @@
+<?php
+
+use Monolog\Handler\StreamHandler;
+
+return [
+
+    'default' => env('LOG_CHANNEL', 'stack'),
+
+    'channels' => [
+
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['stderr'],
+            'ignore_exceptions' => false,
+        ],
+
+        'stderr' => [
+            'driver' => 'monolog',
+            'handler' => StreamHandler::class,
+            'with' => [
+                'stream' => 'php://stderr',
+            ],
+        ],
+
+    ],
+
+];
